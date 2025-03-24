a3e054e071987ebc9335434945b35616c0ea1ff8
iit admin <iit_admin@innmi1srh2-p022.neysaai.infra>
2025-03-24 12:51:57 +0530

    adding metadata and some update

--- build_url.py
+++ build_url.py
@@ -4,8 +4,9 @@ from pathlib import Path
 
 def collect_urls_from_json():
     # Directory containing JSON files of metadata
-    json_dir = Path("/workspace/nfs/nauman/archive/booksbylanguage_urdu")
-    urls = []
+    json_dir = Path("/nfs/nauman/archive/metadata_archive_hindi/chunk1/")
+        # json_file = "/workspace/nfs/shyam/code/download_benchmarks/Archive Org Metadata/booksbylanguage_kannada_archive_data.jsonl"
+    urls = set()
     
     # Iterate through all JSON files in the directory
     for json_file in json_dir.glob("*.jsonl"):
@@ -21,15 +22,15 @@ def collect_urls_from_json():
                 
                 # Build URLs for each identifier
                 for item in items:
-                    if 'identifier' in item:
-                        identifier = item['identifier']
+                    if 'identifier' in item['fields']:
+                        identifier = item['fields']['identifier']
                         url = f"https://archive.org/details/{identifier}"
-                        urls.append(url)
+                        urls.add(url)
         except Exception as e:
             print(f"Error processing {json_file}: {str(e)}")
     
     # Save all URLs to a text file
-    output_file = "archive_urls_urdu.txt"
+    output_file = "./url/archive_urls_hinid_chunk1.txt"
     with open(output_file, 'w', encoding='utf-8') as f:
         for url in urls:
             f.write(url + '\n')
